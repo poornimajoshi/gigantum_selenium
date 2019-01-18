e956807b5c19819d57bfcbf859c029dcd13b9f3a
Poornima Joshi <poornim.joshi@gmail.com>
2019-01-18 13:34:48 -0500

    Some ui-tests for logging in

--- ui_tests/smoke.py
+++ ui_tests/smoke.py
@@ -11,7 +11,7 @@ driver.get("http://localhost:10000/")
 delay = 5
 
 # Click on login button
-#driver.implicitly_wait(10)
+
 try:
     WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.Login_button')))
 except TimeoutException:
@@ -26,3 +26,19 @@ except TimeoutException:
 driver.find_element_by_css_selector("input.auth0-lock-input[name='username']").send_keys('poornimajoshi')
 driver.find_element_by_css_selector("input.auth0-lock-input[name='password']").send_keys('marnIpoo@23')
 driver.find_element_by_class_name('auth0-lock-submit').click()
+driver.implicitly_wait(8)
+try:
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//div[@class='btn--import'][contains(text(),'Create New')]")))
+except TimeoutException:
+    print('Create New button was not visible')
+driver.find_element_by_xpath("//div[@class='btn--import'][contains(text(),'Create New')]").click()
+
+driver.find_element_by_xpath("//input[@placeholder='Enter a unique, descriptive title']").send_keys("a-plot-for-emmys")
+driver.find_element_by_xpath("//textarea[contains(@placeholder,'Briefly describe this Project, its purpose and any other key details.')]").send_keys("A simple demo")
+driver.find_element_by_xpath("//button[contains(text(),'Continue')]").click()
+
+driver.implicitly_wait(3)
+
+# Selecting the python project type and Creating the project
+driver.find_element_by_xpath("//h6[contains(text(),'Python3 Minimal')]").click()
+driver.find_element_by_xpath("//button[contains(@class,'ButtonLoader')]").click()
